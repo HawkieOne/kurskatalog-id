23ec2356586e16f55e72590ccd6b55f99d7e00b5
Håkan <hali0151@student.umu.se>
2022-11-08 14:12:40 +0100

    simple drag n drop
    - can drag but not yet drop
    - other layout
    - prepared for fetching data from database

---
+++ src/components/builder/Year.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { TextVariant } from "../../shared/constants";
+import Text from "../Text";
+import Title from "../Title";
+import DraggableCourse from "./DraggableCourse";
+import DroppableArea from "./DroppableArea";
+
+interface YearProps {
+  year: number;
+}
+export default function Year({ year }: YearProps) {
+  const periods = 4;
+  return (
+    <div className="flex items-center space-x-6">
+      <Title>År {year}</Title>
+      <DraggableCourse />
+      {Array.from(Array(periods).keys()).map((_, index) => (
+        <div className="flex flex-col justify-center items-center" key={index}>
+          <Text size={TextVariant.small}>Läsperiod {index + 1}</Text>
+          {/* <DraggableCourse /> */}
+          <DroppableArea />
+        </div>
+      ))}
+    </div>
+  );
+}
