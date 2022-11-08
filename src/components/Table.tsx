06c7d32fdd7bdd2f3432edf36b053cd4f305a1e0
Ludvig Lindahl <ludde.lindahl@gmail.com>
2022-11-08 12:31:27 +0100

    refactor tables into components
    -change colors to the color scheme used
    -tables now can be initialized with the chosen courses

---
+++ src/components/Table.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Courses from '../pages/Courses';
+import { Course } from "../shared/interfaces";
+
+interface CourseProps {
+    courses: Course[];
+}
+
+export default function Table({ courses }: CourseProps) {
+    return (
+        <div className="overflow-x-auto text-onyx">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th className='bg-cream'>Kursnamn</th>
+                        <th className='bg-cream'>Studietakt</th>
+                        <th className='bg-cream'>Läsperiod</th>
+                        <th className='bg-cream'>Poäng</th>
+                        <th className='bg-cream'>Nivå</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {courses.map((course, index) => (
+                        <tr>
+                            <th className='bg-cream border-none'>{course.name}</th>
+                            <td className='bg-cream border-none'>{course.pace}%</td>
+                            <td className='bg-cream border-none'>{course.startDate}</td>
+                            <td className='bg-cream border-none'>{course.points}hp</td>
+                            <td className='bg-cream border-none'>{course.studyForm}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
