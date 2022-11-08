8e29c0063d14e62aacf1171281ef3de7f14e538e
Restyled.io <commits@restyled.io>
2022-11-08 12:17:32 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content : [ "./src/**/*.{js,jsx,ts,tsx}" ],
-  theme : {
-    extend : {
-      colors : {
-        pink : "#F55065",
-        onyx : "#1F2937",
-        cream : "#FDF2E7",
-        creamDark : "#FBE5CF",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        pink: "#F55065",
+        onyx: "#1F2937",
+        cream: "#FDF2E7",
+        creamDark: "#FBE5CF",
       },
     },
   },
-  plugins : [ require("daisyui") ],
+  plugins: [require("daisyui")],
 };
