18dd0034a871811f0d9c4070c03520c4593b2340
Håkan <hali0151@student.umu.se>
2022-11-11 10:40:36 +0100

    add title for tab

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import Courses from "./pages/Courses";
 import ExamBuilder from "./pages/ExamBuilder";
 import Home from "./pages/Home";
 import MandatoryCourses from "./pages/MandatoryCourses";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <BrowserRouter>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Kurskatalog</title>
+        <link rel="icon" type="image/png" href="icon.ico" sizes="32x32" />
+      </Helmet>
       <div className="h-full flex flex-col">
         <Navbar />
         <div className="grow">
