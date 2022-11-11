171bc15bf3d388ab7eb6f2a1eb9c0cb02b8e6325
Håkan <hali0151@student.umu.se>
2022-11-11 11:24:16 +0100

    add modal for uploading preset
    - add modal with file upload
    - uploaded file is sent to builder
    - can only upload json files
    - should be checked with schema later

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import Modal from "react-modal";
+import FileInput from "./builder/FileInput";
+import Title from "./Title";
+import { ChangeEvent } from "react";
+import { relative } from "path";
+import { Preset } from "../shared/interfaces";
 
-export default function Modal() {
+interface ModalProps {
+  isOpen: boolean;
+  onOpen?: () => void;
+  onCancel: () => void;
+  onSuccess: () => void;
+  onFileUpload: (e: ChangeEvent<HTMLInputElement>) => void;
+  value: Preset | undefined;
+}
+export default function ModalWindow({
+  isOpen,
+  onOpen,
+  onCancel,
+  onSuccess,
+  onFileUpload,
+  value,
+}: ModalProps) {
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  console.log(value?.name.endsWith(".json"))
   return (
-    <div>Modal</div>
-  )
+    <Modal
+      isOpen={isOpen}
+      onAfterOpen={onOpen}
+      contentLabel="Example Modal"
+      style={customStyles}
+    >
+      <div
+        className="btn btn-ghost btn-sm btn-circle absolute right-2 top-2 cursor-pointer"
+        onClick={onCancel}
+      >
+        ✕
+      </div>
+      <div className="h-full flex flex-col items-start p-4 space-y-8 relative">
+        <Title>Ladda upp mall</Title>
+        <FileInput onUpload={onFileUpload} acceptedFormat=".json" />
+        <div className="self-end space-x-4">
+          <button
+            onClick={onCancel}
+            className="btn btn-link no-underline text-red-500"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={onSuccess}
+            className="btn bg-cream text-onyx hover:bg-creamDark border-none"
+            disabled={!value || (value && !value.name.endsWith(".json"))}
+          >
+            Välj mall
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
 }
--- src/components/builder/FileInput.tsx
+++ src/components/builder/FileInput.tsx
@@ -1,22 +1,42 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
+import Text from "../Text";
 
 interface FileInputProps {
+  acceptedFormat: string;
   onUpload: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function FileInput({ onUpload } : FileInputProps) {
+export default function FileInput({ acceptedFormat, onUpload } : FileInputProps) {
+
+  const [errorText, setErrorText] = useState("");
   return (
-    <div className="form-control w-full max-w-xs">
+    <div className="form-control w-full max-w-xs space-y-2">
       <label className="label">
         <span className="label-text">Ladda förinställning</span>
       </label>
       <input
         type="file"
-        accept=".json"
+        accept={acceptedFormat}
         className="file-input file-input-bordered w-full max-w-xs bg-white file:bg-cream
                     file:border-none file:text-pink"
-        onChange={onUpload}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          if (e.target.files?.length === 0) {
+            setErrorText("");
+          } else if (e.target.files?.item(0)?.name.endsWith(acceptedFormat)) {
+            if (e.target.files) {
+              onUpload(e);
+              setErrorText("");
+            } else {
+              setErrorText("Soemthing went wrong uploading the files")
+            }         
+          } else {
+            setErrorText("Invalid file format")
+          }
+        }}
       />
+      <div className="h-12">
+        <Text color="text-pink">{errorText}</Text>
+      </div>
     </div>
   );
 }
