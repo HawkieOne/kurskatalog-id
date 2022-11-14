f6f41f6d31fde15022ae40a3837f097b541f3537
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-11-15 00:55:04 +0100

    continue working with builder
     - installed recoil for state managment
     - trying to combine recoil with react drag n drop
     - not go good

--- src/components/builder/Period.tsx
+++ src/components/builder/Period.tsx
@@ -1,25 +1,29 @@
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { v4 as uuidv4 } from 'uuid';
 import { activeYearState, coursesBuilderSelector, coursesYearState } from '../../atoms/atoms';
+import useCourses from '../../shared/useCourses';
 import Text from "../Text";
 import AddDroppableArea from "./AddDroppableArea";
 import DroppableArea from "./DroppableArea";
 
 interface PeriodProps {
-  number: number;
+  periodIndex: number;
 }
 
-export default function Period({ number }: PeriodProps) {
+export default function Period({ periodIndex }: PeriodProps) {
   const coursesYear = useRecoilValue(coursesYearState);
   const activeYear = useRecoilValue(activeYearState);
   const [courses, setCourses] = useRecoilState(coursesBuilderSelector);
-  const coursesInPeriod = coursesYear.periods[number];
+  const { addCourseToPeriod, removeCoursefromPeriod } = useCourses();
+
+  const coursesInPeriod = coursesYear.periods[periodIndex];
   return (
     <div
       className="flex flex-col justify-start items-center space-y-4"
-      key={number}
+      key={periodIndex}
     >
-      <Text>Läsperiod {number + 1}</Text>
+      <Text>Läsperiod {periodIndex + 1}</Text>
       {/* <DraggableCourse course={TestCourse2} /> */}
       {coursesInPeriod.length > 0 && (
         <div className="h-80 w-full flex flex-col space-y-4">
@@ -30,13 +34,7 @@ export default function Period({ number }: PeriodProps) {
               index={index}
               basis={coursesInPeriod.length > 1 ? "basis-1/2" : "basis-full"}
               onRemove={(index: number) => {
-                const cpyCourses = coursesInPeriod.slice();
-                cpyCourses.splice(index, 1);
-                const cpyCoursesYear = coursesYear;
-                cpyCoursesYear.periods[number] = cpyCourses;
-                const cpyAllCourses = courses;
-                cpyAllCourses[activeYear] = cpyCoursesYear; 
-                setCourses(cpyAllCourses);
+                removeCoursefromPeriod(periodIndex, index);
               }}
             />
           ))}
@@ -45,9 +43,7 @@ export default function Period({ number }: PeriodProps) {
       <AddDroppableArea
         onClick={() => {
           if (coursesInPeriod.length < 6) {
-            const cpyCourses = coursesInPeriod.slice();
-            cpyCourses.push(null);
-            // setCurrentCourses(cpyCourses);
+            addCourseToPeriod(periodIndex);
           }
         }}
       />
