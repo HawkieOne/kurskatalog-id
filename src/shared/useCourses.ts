4d231bac31939e5cfca980ed46f7ed5b1ed36289
Håkan <hali0151@student.umu.se>
2022-11-23 13:38:14 +0100

    feat: implement-saving-changes
    - can save changes
    - method for removing course

--- src/shared/useCourses.ts
+++ src/shared/useCourses.ts
@@ -1,98 +1,51 @@
+import ReactGridLayout from "react-grid-layout";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { activeYearState, coursesBuilderSelector } from "../atoms/atoms";
-import { Course } from "./interfaces";
+import {
+  activeYearState,
+  coursesBuilderSelector,
+  coursesYearState,
+} from "../atoms/atoms";
+import { BuildingBlock, Course } from "./interfaces";
 
 export default function useCourses() {
   const [courses, setCourses] = useRecoilState(coursesBuilderSelector);
   const activeYear = useRecoilValue(activeYearState);
+  const coursesYear = useRecoilValue(coursesYearState);
 
-  // const addCourseToPeriod = (periodIndex: number) => {
-  //   const cpyCourses = courses.slice();
-  //   const cpyPeriods = courses[activeYear].periods.slice();
-  //   const cpyPeriod = cpyPeriods[periodIndex].slice();
-  //   cpyPeriod.push(null);
-  //   cpyPeriods[periodIndex] = cpyPeriod;
-  //   cpyCourses[activeYear] = {
-  //     ...cpyCourses[activeYear],
-  //     periods: cpyPeriods,
-  //   };
-  //   setCourses(cpyCourses);
-  // };
+  const addCourseToPeriod = (periodIndex: number) => {};
 
-  // const removeCoursefromPeriod = (periodIndex: number, courseIndex: number) => {
-  //   const cpyCourses = courses.slice();
-  //   const cpyPeriods = courses[activeYear].periods.slice();
-  //   const cpyPeriod = cpyPeriods[periodIndex].slice();
-  //   cpyPeriod.splice(courseIndex, 1);
-  //   cpyPeriods[periodIndex] = cpyPeriod;
-  //   cpyCourses[activeYear] = {
-  //     ...cpyCourses[activeYear],
-  //     periods: cpyPeriods,
-  //   };
-  //   setCourses(cpyCourses);
-  // };
+  const removeCourse = (uuid: string) => {
+    let cpyCourses = courses.slice();
+    let cpyYearCourses = cpyCourses[activeYear].courses.slice();
+    const uuidIndex = cpyYearCourses.findIndex((e) => e.i === uuid);
+    if (uuidIndex !== -1) {
+      cpyYearCourses.splice(uuidIndex, 1);
+      cpyCourses[activeYear] = {
+        ...cpyCourses[activeYear],
+        courses: cpyYearCourses,
+      };
+      setCourses(cpyCourses);
+    }
+  };
 
-  // const moveCourse = (
-  //   course: Course,
-  //   yearIndex: number | null,
-  //   periodIndex: number,
-  //   courseIndex: number
-  // ) => {
-  //   const cpyCourses = courses.slice();
-  //   var cpyPeriods = courses[activeYear].periods.slice();
-  //   if (yearIndex) {
-  //     cpyPeriods = courses[yearIndex].periods.slice();
-  //   }
-  //   const cpyPeriod = cpyPeriods[periodIndex].slice();
-  //   cpyPeriod[courseIndex] = course;
-  //   cpyPeriods[periodIndex] = cpyPeriod;
-  //   cpyCourses[activeYear] = {
-  //     ...cpyCourses[activeYear],
-  //     periods: cpyPeriods,
-  //   };
-  //   setCourses(cpyCourses);
-  // };
+  const saveChanges = (changedLayout: ReactGridLayout.Layout[]) => {
+    let cpyCourses = courses.slice();
+    const newLayout: BuildingBlock[] = changedLayout.map((course, index) => ({
+      ...course,
+      content: cpyCourses[activeYear].courses.find((e) => e.i === course.i)
+        ?.content,
+    }));
+    cpyCourses[activeYear] = {
+      ...cpyCourses[activeYear],
+      courses: newLayout,
+    };
+    setCourses(cpyCourses);
+  };
 
-  // const switchCourses = (
-  //   courseOne: {
-  //     course: Course;
-  //     yearIndex: number;
-  //     periodIndex: number;
-  //     courseIndex: number;
-  //   },
-  //   courseTwo: {
-  //     course: Course;
-  //     yearIndex: number;
-  //     periodIndex: number;
-  //     courseIndex: number;
-  //   }
-  // ) => {
-  //   const cpyCourses = courses.slice();
-  //   var cpyPeriods = courses[courseOne.yearIndex].periods.slice();
-  //   const cpyPeriod = cpyPeriods[courseOne.periodIndex].slice();
-  //   cpyPeriod[courseOne.courseIndex] = courseTwo.course;
-  //   cpyPeriods[courseOne.periodIndex] = cpyPeriod;
-  //   cpyCourses[courseOne.yearIndex] = {
-  //     ...cpyCourses[courseOne.yearIndex],
-  //     periods: cpyPeriods,
-  //   };
-
-  //   var cpyPeriods2 = cpyCourses[courseTwo.yearIndex].periods.slice();
-  //   const cpyPeriod2 = cpyPeriods[courseTwo.periodIndex].slice();
-  //   cpyPeriod2[courseTwo.courseIndex] = courseOne.course;
-  //   cpyPeriods2[courseTwo.periodIndex] = cpyPeriod2;
-  //   cpyCourses[courseTwo.yearIndex] = {
-  //     ...cpyCourses[courseTwo.yearIndex],
-  //     periods: cpyPeriods2,
-  //   };
-
-  //   setCourses(cpyCourses);
-  // };
-
-  // return {
-  //   addCourseToPeriod,
-  //   removeCoursefromPeriod,
-  //   moveCourse,
-  //   switchCourses,
-  // };
+  return {
+    coursesYear,
+    addCourseToPeriod,
+    removeCourse,
+    saveChanges,
+  };
 }
