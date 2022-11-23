3eab67944a88bc27ec8c8966b7105bf3e9ae5089
Ludvig Lindahl <ludde.lindahl@gmail.com>
2022-11-23 16:25:53 +0100

    add searchbox
    - can now search courses by name, course code and register code
    - the page shows if no courses are found

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Course from "../components/course/Course";
 import Title from "../components/Title";
 import { TitleVariant } from "../shared/constants";
 import coursesJson from '../data/courses.json'
+import Search from "../components/Search";
 
 export default function Courses() {
 
+  const originalCourses = coursesJson;
+  const [searchedCourses, setSearchedCourses] = useState(coursesJson);
+
+
   return (
     <div className="h-full w-full bg-white flex flex-col items-center py-12 space-y-8">
-      <Title>Kurser</Title>
+      <div className="w-3/4 flex justify-center relative">
+        <Title>Kurser</Title>
+        <div className="absolute right-0">
+          <Search onSearch={(searchTerm: string) => {
+            searchTerm = searchTerm.toLowerCase().trim();
+            if (searchTerm === "") {
+              setSearchedCourses(originalCourses);
+              return;
+            }
+            if (searchTerm.length >= 2) {
+              const foundCourses = originalCourses.filter(e =>
+                e.code.toLowerCase().includes(searchTerm) ||
+                e.name.toLowerCase().includes(searchTerm) ||
+                e.registerCode?.toLowerCase().includes(searchTerm));
+              setSearchedCourses(foundCourses);
+            }
+          }} />
+        </div>
+      </div>
       <div className="w-3/4 flex flex-col items-center space-y-6">
-        {coursesJson.map((course, index) => (
+        {searchedCourses.map((course, index) => (
           <Course course={course} key={index} />
         ))}
       </div>
+      {searchedCourses.length === 0 && <p>Inga kurser hittade</p>}
     </div>
   );
 }
