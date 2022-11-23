ec0b0cbf2b065be89b9cd6eed85f427ed262d934
Ludvig Lindahl <ludde.lindahl@gmail.com>
2022-11-23 16:25:53 +0100

    add searchbox
    - can now search courses by name, course code and register code
    - the page shows if no courses are found

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react';
+import { AiOutlineClose } from "react-icons/ai";
 
-export default function Search() {
+interface SearchProps {
+  onSearch: (searchTerm: string) => void;
+}
+
+export default function Search({ onSearch }: SearchProps) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const isInputEmpty = (input: string) => input.length === 0;
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      onSearch(searchTerm)
+    }
+  }
+
   return (
-    <div>Search</div>
+    <div className="form-control">
+      <div className="input-group relative">
+        <div className='relative'>
+          <form id="search-form">
+            <input
+              type="text"
+              placeholder="Sök…"
+              value={searchTerm}
+              className="input input-bordered bg-cream text-onyx justify-self-end rounded-l-xl rounded-none focus:border-onyx focus:outline-none"
+              onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+          </form>
+          {!isInputEmpty(searchTerm) &&
+            <div className='absolute inset-y-0 right-3 flex flex-col justify-center cursor-pointer text-onyx' onClick={() => setSearchTerm("")}>
+              <AiOutlineClose />
+            </div>}
+        </div>
+        <button className="btn btn-square bg-creamDark border-none hover:bg-onyx" onClick={() => onSearch(searchTerm)}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        </button>
+      </div>
+    </div>
   )
 }
