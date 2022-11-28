18ab44adacf1250e9459662edd62cf1ba9b81a46
Restyled.io <commits@restyled.io>
2022-11-28 13:42:02 +0000

    Restyled by whitespace

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -31,7 +31,7 @@ export default function Search({ onSearch }: SearchProps) {
             />
           </form>
           {!isInputEmpty(searchTerm) &&
-            <div className='p-1 absolute top-1/2 -translate-y-1/2 right-3 flex flex-col justify-center cursor-pointer text-onyx hover:bg-onyx hover:text-white hover:rounded-full' 
+            <div className='p-1 absolute top-1/2 -translate-y-1/2 right-3 flex flex-col justify-center cursor-pointer text-onyx hover:bg-onyx hover:text-white hover:rounded-full'
                 onClick={() => setSearchTerm("")}>
               <AiOutlineClose />
             </div>}
