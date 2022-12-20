d40784dc244ae5d10a56f0e5cc018d499f002bd7
Håkan <hali0151@student.umu.se>
2022-12-20 22:31:43 +0100

    fix: remove horizontal scrollbar

--- src/pages/builder/Drawer.tsx
+++ src/pages/builder/Drawer.tsx
@@ -16,7 +16,7 @@ export default function Drawer({ side, refPointer, children }: DrawerProps) {
       transition={{ ease: "linear" }}
       exit={{ x: side === "left" ? -350 : 350 }}
       layout
-      className={`max-h-screen overflow-y-auto inset-y-0 absolute bg-white shadow-lg ${side === "left" ? "left-0 w-80" : "right-0"
+      className={`max-h-screen overflow-y-auto inset-y-0 absolute bg-white shadow-lg ${side === "left" ? "left-0 w-96" : "right-0"
         } flex flex-col z-30 print:hidden`}
       ref={refPointer}
     >
