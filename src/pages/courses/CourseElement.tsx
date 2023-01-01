224d7e8c679af2aa6129f6f6f74e67a2fe0a6a02
Håkan <hali0151@student.umu.se>
2023-01-01 18:26:32 +0100

    feat: animate sorting
      * animations with framer motion

--- src/pages/courses/CourseElement.tsx
+++ src/pages/courses/CourseElement.tsx
@@ -4,6 +4,7 @@ import Title from "../../components/Title";
 import { TextVariants, TitleVariants } from "../../shared/constants";
 import { Course } from "../../shared/interfaces";
 import ColorSquare from "./ColorSquare";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface CourseProps {
   course: Course;
@@ -12,34 +13,42 @@ interface CourseProps {
 export default function CourseElement({ course }: CourseProps) {
   const navigate = useNavigate();
   return (
-    <div
-      className="w-full flex justify-between p-3 rounded-md even:hover:bg-ashGrey odd:hover:bg-boneGrey 
+    <AnimatePresence>
+      <motion.div
+        layout
+        key={course.code}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+        exit={{ opacity: 0 }}
+        className="w-full flex justify-between p-3 rounded-md even:hover:bg-ashGrey odd:hover:bg-boneGrey 
                     cursor-pointer"
-      onClick={() =>
-        navigate(course.name, {
-          state: { course: course },
-        })
-      }
-    >
-      <div className="basis-3/4 flex flex-col justify-evenly">
-        <Title size={TitleVariants.small}>{course.name}</Title>
-        <Text>{course.code}</Text>
-        <div className="w-2/4 flex space-x-4">
-          {course.location && (
-            <Text
-              size={TextVariants.small}
-              color={"text-midnightGreenEagleGreen"}
-            >
-              {course.location}
-            </Text>
-          )}
-          <Text size={TextVariants.small}>{course.level}</Text>
+        onClick={() =>
+          navigate(course.name, {
+            state: { course: course },
+          })
+        }
+      >
+        <div className="basis-3/4 flex flex-col justify-evenly">
+          <Title size={TitleVariants.small}>{course.name}</Title>
+          <Text>{course.code}</Text>
+          <div className="w-2/4 flex space-x-4">
+            {course.location && (
+              <Text
+                size={TextVariants.small}
+                color={"text-midnightGreenEagleGreen"}
+              >
+                {course.location}
+              </Text>
+            )}
+            <Text size={TextVariants.small}>{course.level}</Text>
+          </div>
         </div>
-      </div>
-      <div className="basis-1/5 flex justify-end space-x-4 text-w½hite p-8">
-        {course.points && <ColorSquare value={course.points} text="hp" />}
-        {course.pace && <ColorSquare value={course.pace} text="%" />}
-      </div>
-    </div>
+        <div className="basis-1/5 flex justify-end space-x-4 text-w½hite p-8">
+          {course.points && <ColorSquare value={course.points} text="hp" />}
+          {course.pace && <ColorSquare value={course.pace} text="%" />}
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
