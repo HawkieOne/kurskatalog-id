03c471e65267aea89385ea6c45382c3fd8db0002
Håkan <hali0151@student.umu.se>
2023-01-01 15:10:56 +0100

    feat: fix left drawer
      * left drawer takes upp 100% of height
      * fix color of collapse component

--- src/pages/builder/Drawer.tsx
+++ src/pages/builder/Drawer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AnimatePresence, motion } from "framer-motion"
+import { motion } from "framer-motion"
 
 interface DrawerProps {
   side: "left" | "right";
@@ -16,7 +16,7 @@ export default function Drawer({ side, refPointer, children }: DrawerProps) {
       transition={{ ease: "linear" }}
       exit={{ x: side === "left" ? -350 : 350 }}
       layout
-      className={`max-h-screen overflow-y-auto inset-y-0 absolute bg-white shadow-lg ${side === "left" ? "left-0 w-96" : "right-0"
+      className={`h-100 overflow-y-auto inset-y-0 absolute bg-white shadow-lg ${side === "left" ? "left-0 w-96" : "right-0"
         } flex flex-col z-30 print:hidden`}
       ref={refPointer}
     >
